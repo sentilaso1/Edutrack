5a244d8405722b90bf0be20cde3a51d9a020ed10
nguyenhoanvn <hoanyu12345@gmail.com>
2025-05-22 02:17:25 +0700

    Add a page to view mentor list, and a filter for it.

---
+++ src/main/java/com/example/edutrack/accounts/service/MentorService.java
@@ -0,0 +1,24 @@
+package com.example.edutrack.accounts.service;
+
+import com.example.edutrack.accounts.model.Mentor;
+import com.example.edutrack.accounts.repository.MentorRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MentorService {
+    private final MentorRepository mentorRepository;
+
+    public MentorService(MentorRepository mentorRepository) {
+        this.mentorRepository = mentorRepository;
+    }
+
+    public List<Mentor> getAllMentors() {
+        return mentorRepository.findAll();
+    }
+
+    public List<Mentor> searchMentors(String name, String expertise, Double rating, Integer totalSessions, Boolean isAvailable) {
+        return mentorRepository.searchMentors(name, expertise, rating, totalSessions, isAvailable);
+    }
+}
