4ab9ab1d52ec066be2d7957e25b960068dbf269f
Pham Tung Duong <phamtungduong763@gmail.com>
2025-05-25 14:21:11 +0700

    Feature: add repo and service for user management

--- src/main/java/com/example/edutrack/accounts/repository/UserRepository.java
+++ src/main/java/com/example/edutrack/accounts/repository/UserRepository.java
@@ -1,12 +1,28 @@
 package com.example.edutrack.accounts.repository;
 
-import com.example.edutrack.accounts.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.example.edutrack.accounts.model.User;
+
 public interface UserRepository extends JpaRepository<User, UUID> {
+        @Query("SELECT u FROM User u " +
+           "WHERE (:email IS NULL OR LOWER(u.email) LIKE LOWER(CONCAT('%', :email, '%'))) " +
+           "AND (:fullName IS NULL OR LOWER(u.fullName) LIKE LOWER(CONCAT('%', :fullName, '%'))) " +
+           "AND (:isLocked IS NULL OR u.isLocked = :isLocked) " +
+           "AND (:isActive IS NULL OR u.isActive = :isActive)")
+    List<User> searchUsers(
+            @Param("email") String email,
+            @Param("fullName") String fullName,
+            @Param("isLocked") Boolean isLocked,
+            @Param("isActive") Boolean isActive
+    );
+    
     Optional<User> findByEmail(String email);
     Optional<User> findByResetToken(String token);
 
