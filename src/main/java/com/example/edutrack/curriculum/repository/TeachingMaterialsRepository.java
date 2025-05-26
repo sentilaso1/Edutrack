780be9a1389260b915a9b8f021e9376b34dea6dc
Nguyen Thanh Vinh <nguyenthanhvinh07052005@gmail.com>
2025-05-26 16:55:40 +0700

    Thay đổi về course, xoá trường duyệt course

--- src/main/java/com/example/edutrack/curriculum/repository/TeachingMaterialsRepository.java
+++ src/main/java/com/example/edutrack/curriculum/repository/TeachingMaterialsRepository.java
@@ -1,10 +1,14 @@
 package com.example.edutrack.curriculum.repository;
 
-import com.example.edutrack.curriculum.model.TeachingMaterial;
+import com.example.edutrack.curriculum.model.TeachingMaterials;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.UUID;
+
 @Repository
-public interface TeachingMaterialsRepository extends CrudRepository<TeachingMaterial, Integer> {
+public interface TeachingMaterialsRepository extends CrudRepository<TeachingMaterials, Integer> {
+    List<TeachingMaterials> findByCourseId(UUID courseId);
 
 }
