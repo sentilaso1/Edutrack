1b7e82d1c32a4975befbc1d450f86c5f12b7eb3e
Bui Viet Dung <buidungnd.2005@gmail.com>
2025-05-28 01:43:05 +0700

    feat: added support for bookmark sorting and filtering.

--- src/main/java/com/example/edutrack/accounts/service/implementations/BookmarkServiceImpl.java
+++ src/main/java/com/example/edutrack/accounts/service/implementations/BookmarkServiceImpl.java
@@ -5,18 +5,15 @@ import com.example.edutrack.accounts.model.Bookmark;
 import com.example.edutrack.accounts.model.User;
 import com.example.edutrack.accounts.repository.BookmarkRepository;
 import com.example.edutrack.accounts.service.interfaces.BookmarkService;
-import com.example.edutrack.curriculum.dto.TagDTO;
-import com.example.edutrack.curriculum.model.CourseTag;
 import com.example.edutrack.curriculum.model.Tag;
-import com.example.edutrack.curriculum.service.implementation.CourseTagServiceImpl;
 import com.example.edutrack.curriculum.service.implementation.TagServiceImpl;
-import com.example.edutrack.curriculum.service.interfaces.CourseTagService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class BookmarkServiceImpl implements BookmarkService {
@@ -30,25 +27,89 @@ public class BookmarkServiceImpl implements BookmarkService {
     }
 
     @Override
-    public Page<Bookmark> findAllBookmarks(Pageable pageable) {
+    public Page<Bookmark> findAllBookmarksDateDesc() {
+        return bookmarkRepository.findAllByOrderByCreatedDateDesc(Pageable.unpaged());
+    }
+
+    @Override
+    public Page<Bookmark> findAllBookmarksDateAsc() {
+        return bookmarkRepository.findAllByOrderByCreatedDateAsc(Pageable.unpaged());
+    }
+
+    @Override
+    public Page<Bookmark> findAllBookmarksDateDesc(Pageable pageable) {
         return bookmarkRepository.findAllByOrderByCreatedDateDesc(pageable);
     }
 
     @Override
-    public Page<Bookmark> findAllBookmarks(Pageable pageable, User user) {
+    public Page<Bookmark> findAllBookmarksDateDesc(Pageable pageable, User user) {
         return bookmarkRepository.findAllByUserOrderByCreatedDateDesc(pageable, user);
     }
 
     @Override
-    public Page<BookmarkDTO> findAllBookmarkWithCourseTags(Pageable pageable) {
-        Page<Bookmark> bookmarkPage = this.findAllBookmarks(pageable);
+    public Page<Bookmark> findAllBookmarksDateAsc(Pageable pageable) {
+        return bookmarkRepository.findAllByOrderByCreatedDateAsc(pageable);
+    }
+
+    @Override
+    public Page<Bookmark> findAllBookmarksDateAsc(Pageable pageable, User user) {
+        return bookmarkRepository.findAllByUserOrderByCreatedDateAsc(pageable, user);
+    }
+
+    @Override
+    public Page<BookmarkDTO> findAllBookmarkWithCourseTagsDateDesc(Pageable pageable) {
+        Page<Bookmark> bookmarkPage = this.findAllBookmarksDateDesc(pageable);
         return getBookmarkDTOS(bookmarkPage);
     }
 
     @Override
-    public Page<BookmarkDTO> findAllBookmarkWithCourseTags(Pageable pageable, User user) {
-        Page<Bookmark> bookmarkPage = this.findAllBookmarks(pageable, user);
+    public Page<BookmarkDTO> findAllBookmarkWithCourseTagsDateDesc(Pageable pageable, User user) {
+        Page<Bookmark> bookmarkPage = this.findAllBookmarksDateDesc(pageable, user);
         return getBookmarkDTOS(bookmarkPage);
+    }
+
+    @Override
+    public Page<BookmarkDTO> findAllBookmarkWithCourseTagsDateAsc(Pageable pageable) {
+        Page<Bookmark> bookmarkPage = this.findAllBookmarksDateAsc(pageable);
+        return getBookmarkDTOS(bookmarkPage);
+    }
+
+    @Override
+    public Page<BookmarkDTO> findAllBookmarkWithCourseTagsDateAsc(Pageable pageable, User user) {
+        Page<Bookmark> bookmarkPage = this.findAllBookmarksDateAsc(pageable, user);
+        return getBookmarkDTOS(bookmarkPage);
+    }
+
+    @Override
+    public Page<BookmarkDTO> findAllBookmarkContainingTagsDateDesc(Pageable pageable, List<Integer> tagIds) {
+        Page<Bookmark> bookmarkPage = bookmarkRepository.findAllContainingTagCreatedDateDesc(tagIds, pageable);
+        return getBookmarkDTOS(bookmarkPage);
+    }
+
+    @Override
+    public Page<BookmarkDTO> findAllBookmarkContainingTagsDateDesc(Pageable pageable, User user, List<Integer> tagIds) {
+        Page<Bookmark> bookmarkPage = bookmarkRepository.findAllByUserContainingTagCreatedDateDesc(tagIds, user.getId(), pageable);
+        return getBookmarkDTOS(bookmarkPage);
+    }
+
+    @Override
+    public Page<BookmarkDTO> findAllBookmarkContainingTagsDateAsc(Pageable pageable, List<Integer> tagIds) {
+        Page<Bookmark> bookmarkPage = bookmarkRepository.findAllContainingTagCreatedDateAsc(tagIds, pageable);
+        return getBookmarkDTOS(bookmarkPage);
+    }
+
+    @Override
+    public Page<BookmarkDTO> findAllBookmarkContainingTagsDateAsc(Pageable pageable, User user, List<Integer> tagIds) {
+        Page<Bookmark> bookmarkPage = bookmarkRepository.findAllByUserContainingTagCreatedDateAsc(tagIds, user.getId(), pageable);
+        return getBookmarkDTOS(bookmarkPage);
+    }
+
+    @Override
+    public List<Tag> findAllUniqueTags(List<BookmarkDTO> bookmarkDTOs) {
+        return bookmarkDTOs.stream()
+                .flatMap(bookmarkDTO -> bookmarkDTO.getTags().stream())
+                .distinct()
+                .toList();
     }
 
     private Page<BookmarkDTO> getBookmarkDTOS(Page<Bookmark> bookmarkPage) {
@@ -56,8 +117,8 @@ public class BookmarkServiceImpl implements BookmarkService {
             BookmarkDTO bookmarkDTO = new BookmarkDTO();
             bookmarkDTO.setBookmark(bookmark);
 
-            List<Tag> tagDtos = tagServiceImpl.findTagsByCourseId(bookmark.getCourse().getId());
-            bookmarkDTO.setTags(tagDtos);
+            List<Tag> tags = tagServiceImpl.findTagsByCourseId(bookmark.getCourse().getId());
+            bookmarkDTO.setTags(tags);
 
             return bookmarkDTO;
         });
