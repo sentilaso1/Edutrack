1ac70f09f4232975f9ca31c0784b4d45a407c7d3
Bui Viet Dung <buidungnd.2005@gmail.com>
2025-05-28 01:43:33 +0700

    feat: added bookmark sorting and filtering.

--- src/main/java/com/example/edutrack/accounts/controller/BookmarkController.java
+++ src/main/java/com/example/edutrack/accounts/controller/BookmarkController.java
@@ -1,8 +1,10 @@
 package com.example.edutrack.accounts.controller;
 
 import com.example.edutrack.accounts.dto.BookmarkDTO;
+import com.example.edutrack.accounts.dto.BookmarkFilterForm;
 import com.example.edutrack.accounts.model.Bookmark;
 import com.example.edutrack.accounts.service.interfaces.BookmarkService;
+import com.example.edutrack.curriculum.model.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -10,7 +12,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
 
 @Controller
 public class BookmarkController {
@@ -24,16 +29,39 @@ public class BookmarkController {
     }
 
     @GetMapping("/bookmark/list/{page}")
-    public String bookmarkList(Model model, @PathVariable int page) {
+    public String bookmarkList(Model model, @PathVariable int page, @ModelAttribute BookmarkFilterForm params) {
         if (page - 1 < 0) {
             return "redirect:/404";
         }
 
         model.addAttribute("pageNumber", page);
         Pageable pageable = PageRequest.of(page - 1, PAGE_SIZE);
+        Page<BookmarkDTO> bookmarkPage = null;
 
-        Page<BookmarkDTO> bookmarkPage = bookmarkService.findAllBookmarkWithCourseTags(pageable);
+        // TODO: Optimize this logic to avoid redundant queries
+        List<Tag> tags = null;
+        List<BookmarkDTO> bookmarks = null;
+
+        if (params.getSort() == null || params.getSort().equals(BookmarkFilterForm.SORT_DATE_DESC)) {
+            if (params.getTags() == null || params.getTags().isEmpty()) {
+                bookmarkPage = bookmarkService.findAllBookmarkWithCourseTagsDateDesc(pageable);
+            } else {
+                bookmarkPage = bookmarkService.findAllBookmarkContainingTagsDateDesc(pageable, params.getTags());
+            }
+            bookmarks = bookmarkService.findAllBookmarkWithCourseTagsDateDesc(Pageable.unpaged()).getContent();
+            tags = bookmarkService.findAllUniqueTags(bookmarks);
+        } else {
+            if (params.getTags() == null || params.getTags().isEmpty()) {
+                bookmarkPage = bookmarkService.findAllBookmarkWithCourseTagsDateAsc(pageable);
+            } else {
+                bookmarkPage = bookmarkService.findAllBookmarkContainingTagsDateAsc(pageable, params.getTags());
+            }
+            bookmarks = bookmarkService.findAllBookmarkWithCourseTagsDateAsc(Pageable.unpaged()).getContent();
+            tags = bookmarkService.findAllUniqueTags(bookmarks);
+        }
+
         model.addAttribute("page", bookmarkPage);
+        model.addAttribute("tags", tags);
 
         return "/bookmarks/list-bookmark";
     }
