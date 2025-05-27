e713b0fbeef81ee27b27cd0e9aac32349da1786c
Nguyen Thanh Vinh <nguyenthanhvinh07052005@gmail.com>
2025-05-27 13:39:56 +0700

    Update tag system and course-tag relationships

--- src/main/java/com/example/edutrack/curriculum/repository/TagRepository.java
+++ src/main/java/com/example/edutrack/curriculum/repository/TagRepository.java
@@ -4,9 +4,12 @@ import com.example.edutrack.curriculum.model.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface TagRepository extends JpaRepository<Tag, Integer> {
     Optional<Tag> findByTitle(String title);
+    List<Tag> findByCourseId(UUID courseId);
 }
--- src/main/java/com/example/edutrack/curriculum/service/implementation/TagServiceImpl.java
+++ src/main/java/com/example/edutrack/curriculum/service/implementation/TagServiceImpl.java
@@ -1,11 +1,13 @@
 package com.example.edutrack.curriculum.service.implementation;
 
+import com.example.edutrack.curriculum.model.CourseTag;
 import com.example.edutrack.curriculum.model.Tag;
 import com.example.edutrack.curriculum.repository.TagRepository;
 import com.example.edutrack.curriculum.service.interfaces.TagService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class TagServiceImpl implements TagService {
@@ -18,5 +20,10 @@ public class TagServiceImpl implements TagService {
     @Override
     public List<Tag> findAll() {
         return tagRepository.findAll();
+    }
+
+    @Override
+    public List<Tag> findTagsByCourseId(UUID courseId) {
+        return tagRepository.findByCourseId(courseId);
     }
 }
