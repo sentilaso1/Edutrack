1de561b841eea508ce65309168d0c6923c157246
Bui Viet Dung <buidungnd.2005@gmail.com>
2025-06-06 21:06:08 +0700

    feat: created the Wallet and Transaction entity.

---
+++ src/main/java/com/example/edutrack/transactions/service/interfaces/WalletService.java
@@ -0,0 +1,13 @@
+package com.example.edutrack.transactions.service.interfaces;
+
+import com.example.edutrack.accounts.model.User;
+import com.example.edutrack.transactions.model.Wallet;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface WalletService {
+    Optional<Wallet> findByUser(User user);
+    Optional<Wallet> findById(UUID id);
+    Wallet save(User user);
+}
