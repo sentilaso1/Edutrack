309365c9131624b7e4e9db56b613af2532be3c44
Nguyen Thanh Vinh <nguyenthanhvinh07052005@gmail.com>
2025-06-07 01:49:44 +0700

    Finish Get Popular Tags In Landing Page

--- src/main/java/com/example/edutrack/curriculum/service/interfaces/CourseTagService.java
+++ src/main/java/com/example/edutrack/curriculum/service/interfaces/CourseTagService.java
@@ -1,6 +1,7 @@
 package com.example.edutrack.curriculum.service.interfaces;
 
 import com.example.edutrack.curriculum.dto.TagDTO;
+import com.example.edutrack.curriculum.dto.TagEnrollmentCountDTO;
 import com.example.edutrack.curriculum.model.CourseTag;
 import com.example.edutrack.curriculum.model.Tag;
 
@@ -10,4 +11,8 @@ import java.util.UUID;
 public interface CourseTagService {
     boolean existsByCourseAndTag(UUID courseId, int tagId);
     void addCourseTag(UUID courseId, int tagId);
+
+    List<TagEnrollmentCountDTO> getTopTags(int maxCount);
+
+    List<Tag> getAllTags();
 }
