ed95605ec2dd4861d3db2d14f2b39b2bb59f1dcb
Bui Viet Dung <buidungnd.2005@gmail.com>
2025-06-09 17:20:55 +0700

    fix: missing service annotations.

--- src/main/java/com/example/edutrack/transactions/service/TransactionServiceImpl.java
+++ src/main/java/com/example/edutrack/transactions/service/TransactionServiceImpl.java
@@ -1,16 +1,19 @@
-package com.example.edutrack.transactions.service.interfaces;
+package com.example.edutrack.transactions.service;
 
 import com.example.edutrack.accounts.model.User;
 import com.example.edutrack.transactions.model.Transaction;
 import com.example.edutrack.transactions.model.Wallet;
 import com.example.edutrack.transactions.repository.TransactionRepository;
+import com.example.edutrack.transactions.service.interfaces.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public class TransactionServiceImpl implements TransactionService{
+@Service
+public class TransactionServiceImpl implements TransactionService {
     private final TransactionRepository transactionRepository;
 
     @Autowired
