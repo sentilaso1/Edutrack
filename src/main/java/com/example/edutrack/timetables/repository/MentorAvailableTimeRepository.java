e06ae14da52e43b1535ec9ea1072504043d1a058
Nguyen Thanh Vinh <nguyenthanhvinh07052005@gmail.com>
2025-06-09 14:19:45 +0700

    Fix error related to EnrollmentRepository

--- src/main/java/com/example/edutrack/timetables/repository/MentorAvailableTimeRepository.java
+++ src/main/java/com/example/edutrack/timetables/repository/MentorAvailableTimeRepository.java
@@ -6,8 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 public interface MentorAvailableTimeRepository extends JpaRepository<MentorAvailableTime, MentorAvailableTimeId> {
-    List<MentorAvailableTime> findByMentorId(int id);
+    List<MentorAvailableTime> findByMentorId(UUID id);
 }
