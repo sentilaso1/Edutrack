293996a42c093b7c6f51b35e70d6221aca88af36
lephuonglinga <142556541+lephuonglinga@users.noreply.github.com>
2025-06-10 16:37:56 +0700

    refacter: add EnrollmentSchedule attendance

--- src/main/java/com/example/edutrack/timetables/model/EnrollmentSchedule.java
+++ src/main/java/com/example/edutrack/timetables/model/EnrollmentSchedule.java
@@ -8,6 +8,15 @@ import java.util.Date;
 @Entity
 @Table(name = "enrollment_schedule")
 public class EnrollmentSchedule {
+    public enum Attendance {
+        NOT_YET, PRESENT, ABSENT, CANCELLED;
+
+        @Override
+        public String toString() {
+            return name().toLowerCase();
+        }
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
@@ -29,8 +38,84 @@ public class EnrollmentSchedule {
     @Column(name = "score")
     private Integer score;
 
+    @Column(name = "attendance")
+    private Attendance attendance = Attendance.NOT_YET;
+
     public EnrollmentSchedule() {
     }
 
+    public EnrollmentSchedule(Enrollment enrollment, Slot slot, LocalDate date) {
+        this.enrollment = enrollment;
+        this.slot = slot;
+        this.date = date;
+    }
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Enrollment getEnrollment() {
+        return enrollment;
+    }
+
+    public void setEnrollment(Enrollment enrollment) {
+        this.enrollment = enrollment;
+    }
+
+    public Slot getSlot() {
+        return slot;
+    }
+
+    public void setSlot(Slot slot) {
+        this.slot = slot;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Boolean getTest() {
+        return isTest;
+    }
+
+    public void setTest(Boolean test) {
+        isTest = test;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+    public Attendance getAttendance() {
+        return attendance;
+    }
+
+    public void setAttendance(Attendance attendance) {
+        this.attendance = attendance;
+    }
+
+    @Override
+    public String toString() {
+        return "EnrollmentSchedule{" +
+               "id=" + id +
+               ", enrollment=" + enrollment +
+               ", slot=" + slot +
+               ", date=" + date +
+               ", isTest=" + isTest +
+               ", score=" + score +
+               ", attendance=" + attendance +
+               '}';
+    }
 }
