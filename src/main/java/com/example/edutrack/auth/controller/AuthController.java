ea48de59dd95709f61ca052103d70562edd141b4
lephuonglinga <142556541+lephuonglinga@users.noreply.github.com>
2025-06-11 23:02:25 +0700

    fix: fix logout

--- src/main/java/com/example/edutrack/auth/controller/AuthController.java
+++ src/main/java/com/example/edutrack/auth/controller/AuthController.java
@@ -173,6 +173,6 @@ public class AuthController {
     @GetMapping("/logout")
     public String logout(HttpSession session) {
         session.invalidate();
-        return "redirect:/auth/login";
+        return "redirect:/login";
     }
 }
