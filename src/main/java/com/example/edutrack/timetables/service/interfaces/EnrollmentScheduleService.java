2f7737938df7e77d628c21edd66361b5c9948a54
lephuonglinga <142556541+lephuonglinga@users.noreply.github.com>
2025-06-12 14:24:22 +0700

    feat: add model for detail teaching slot

--- src/main/java/com/example/edutrack/timetables/service/interfaces/EnrollmentScheduleService.java
+++ src/main/java/com/example/edutrack/timetables/service/interfaces/EnrollmentScheduleService.java
@@ -5,6 +5,7 @@ import com.example.edutrack.accounts.model.Mentor;
 import com.example.edutrack.accounts.model.User;
 import com.example.edutrack.curriculum.model.CourseMentor;
 import com.example.edutrack.timetables.model.Day;
+import com.example.edutrack.timetables.model.Enrollment;
 import com.example.edutrack.timetables.model.EnrollmentSchedule;
 import com.example.edutrack.timetables.model.Slot;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -20,4 +21,6 @@ public interface EnrollmentScheduleService {
     List<EnrollmentSchedule> findAll();
 
     List<EnrollmentSchedule> findByMentorAndDateBetween(Mentor mentor, LocalDate weekStart, LocalDate weekEnd);
+
+    EnrollmentSchedule findById(Integer esid);
 }
