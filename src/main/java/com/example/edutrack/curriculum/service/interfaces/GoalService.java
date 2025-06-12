901b7822a10e9bd55849a36f4ea386859fced715
Nguyen Thanh Vinh <nguyenthanhvinh07052005@gmail.com>
2025-06-12 17:23:21 +0700

    Update: Add goal entity for mentee's learning tracker page, change controller and service related to it

---
+++ src/main/java/com/example/edutrack/curriculum/service/interfaces/GoalService.java
@@ -0,0 +1,14 @@
+package com.example.edutrack.curriculum.service.interfaces;
+
+import com.example.edutrack.curriculum.model.Goal;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface GoalService {
+    void saveGoal(UUID menteeId, Goal goal);
+
+    List<Goal> getGoalsByMentee(UUID menteeId);
+
+    void updateGoalStatus(UUID goalId, Goal.Status newStatus, UUID menteeId);
+}
