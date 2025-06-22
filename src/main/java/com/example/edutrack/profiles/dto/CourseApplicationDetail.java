319d2166ffff0daf9c9d21ec983361d250997b2e
nguyenhoanvn <hoanyu12345@gmail.com>
2025-06-22 16:51:33 +0700

    resolve conflict

---
+++ src/main/java/com/example/edutrack/profiles/dto/CourseApplicationDetail.java
@@ -0,0 +1,22 @@
+package com.example.edutrack.profiles.dto;
+
+public class CourseApplicationDetail {
+    private double price;
+    private String description;
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
