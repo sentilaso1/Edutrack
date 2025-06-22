25af7a92a836dcaeda00c810a8329ab8400801db
Bui Viet Dung <buidungnd.2005@gmail.com>
2025-06-22 12:50:51 +0700

    refactor: added status to mentor available time.

--- src/main/java/com/example/edutrack/timetables/model/MentorAvailableTime.java
+++ src/main/java/com/example/edutrack/timetables/model/MentorAvailableTime.java
@@ -2,12 +2,27 @@ package com.example.edutrack.timetables.model;
 
 import com.example.edutrack.accounts.model.Mentor;
 import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDate;
+import java.util.Date;
 
 @Entity
 @Table(name = "mentor_available_time")
 public class MentorAvailableTime {
+    public enum Status {
+        PENDING,
+        APPROVED,
+        REJECTED,
+        DRAFT,
+        CANCELLED;
+
+        @Override
+        public String toString() {
+            return this.name().toLowerCase();
+        }
+    }
+
     @EmbeddedId
     private MentorAvailableTimeId id;
 
@@ -15,6 +30,17 @@ public class MentorAvailableTime {
     @MapsId("mentorId")
     @JoinColumn(name = "mentor_id")
     private Mentor mentor;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private Status status = Status.DRAFT;
+
+    @Column(name = "reason")
+    private String reason;
+
+    @Column(name = "created_date")
+    @CreatedDate
+    private Date createdDate = new Date();
 
     public MentorAvailableTime() {
     }
@@ -40,11 +66,38 @@ public class MentorAvailableTime {
         this.mentor = mentor;
     }
 
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
     @Override
     public String toString() {
         return "MentorAvailableTime{" +
                 "id=" + id +
                 ", mentor=" + mentor +
+                ", status=" + status +
+                ", reason='" + reason + '\'' +
+                ", createdDate=" + createdDate +
                 '}';
     }
 }
