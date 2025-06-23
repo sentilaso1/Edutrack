afe42b056e2fe0132d88f603d166c6a6405b9351
lephuonglinga <142556541+lephuonglinga@users.noreply.github.com>
2025-06-23 15:00:33 +0700

    feat: approve function skill request by manager

--- src/main/java/com/example/edutrack/accounts/controller/ManagerController.java
+++ src/main/java/com/example/edutrack/accounts/controller/ManagerController.java
@@ -1,18 +1,28 @@
 package com.example.edutrack.accounts.controller;
 
+import com.example.edutrack.accounts.model.Mentor;
 import com.example.edutrack.accounts.service.interfaces.MenteeService;
 import com.example.edutrack.accounts.service.interfaces.MentorService;
 import com.example.edutrack.timetables.dto.MentorAvailableTimeDTO;
+import com.example.edutrack.timetables.dto.RequestedSchedule;
+import com.example.edutrack.timetables.model.Day;
 import com.example.edutrack.timetables.model.Enrollment;
+import com.example.edutrack.timetables.model.Slot;
+import com.example.edutrack.timetables.service.implementation.EnrollmentScheduleServiceImpl;
 import com.example.edutrack.timetables.service.implementation.EnrollmentServiceImpl;
+import com.example.edutrack.timetables.service.interfaces.EnrollmentScheduleService;
 import com.example.edutrack.timetables.service.interfaces.EnrollmentService;
 import com.example.edutrack.timetables.service.interfaces.MentorAvailableTimeService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -21,13 +31,19 @@ public class ManagerController {
     private final MenteeService menteeService;
     private final MentorAvailableTimeService mentorAvailableTimeService;
     private final EnrollmentService enrollmentService;
+    private final EnrollmentScheduleService enrollmentScheduleService;
 
     @Autowired
-    public ManagerController(MentorService mentorService, MenteeService menteeService, MentorAvailableTimeService mentorAvailableTimeService, EnrollmentService enrollmentServiceImpl) {
+    public ManagerController(MentorService mentorService,
+                             MenteeService menteeService,
+                             MentorAvailableTimeService mentorAvailableTimeService,
+                             EnrollmentService enrollmentServiceImpl,
+                             EnrollmentScheduleService enrollmentScheduleService) {
         this.mentorService = mentorService;
         this.menteeService = menteeService;
         this.mentorAvailableTimeService = mentorAvailableTimeService;
         this.enrollmentService = enrollmentServiceImpl;
+        this.enrollmentScheduleService = enrollmentScheduleService;
     }
 
     @GetMapping("/manager/dashboard")
@@ -67,6 +83,43 @@ public class ManagerController {
         List<Enrollment> enrollmentRequests = enrollmentService.findAllApprovedEnrollments();
         model.addAttribute("enrollmentRequests", enrollmentRequests);
         return "manager/create-class";
+    }
+
+    @GetMapping("manager/create-class/{eid}")
+    public String actionCreateClass(@PathVariable("eid") long eid,
+                                    @RequestParam String action) {
+        Enrollment enrollment = enrollmentService.findById(eid);
+        if (enrollment == null) {
+            return "redirect:/mentor/schedule?error=enrollmentNotFound";
+        }
+        if (action.equals("reject")) {
+            enrollment.setStatus(Enrollment.EnrollmentStatus.REJECTED);
+            enrollmentService.save(enrollment);
+            return "redirect:/manager/create-class?status=REJECTED&reject=" + eid;
+        }
+        if(action.equals("approve")){
+            enrollment.setStatus(Enrollment.EnrollmentStatus.CREATED);
+            enrollmentScheduleService.saveEnrollmentSchedule(enrollment);
+            return "redirect:/manager/create-class?status=APPROVED&approve=" + eid;
+        }if(action.equals("view")){
+            return "redirect:/manager/create-class/" + eid + "/view";
+        }
+        return "redirect:/manager/create-class";
+    }
+
+    @GetMapping("/manager/create-class/{eid}/view")
+    public String viewSensorClass(@PathVariable Long eid,
+                                  Model model) {
+        Enrollment enrollment = enrollmentService.findById(eid);
+        List<Slot> slots = new ArrayList<>();
+        List<Day> days = new ArrayList<>();
+        String summary = enrollment.getScheduleSummary();
+
+        EnrollmentScheduleServiceImpl.parseDaySlotString(summary, days, slots);
+
+        List<RequestedSchedule> startTime = enrollmentScheduleService.findStartLearningTime(enrollment.getMentee(), enrollment.getCourseMentor(), slots, days, enrollment.getTotalSlots());
+        model.addAttribute("startTime", startTime);
+        return "manager/skill-register-request-detail";
     }
 }
 
