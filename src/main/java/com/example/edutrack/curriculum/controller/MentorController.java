4e9b7ceaafb070029c04ab0d6c59e8e2245a9bdb
nguyenhoanvn <hoanyu12345@gmail.com>
2025-06-26 05:27:38 +0700

    - Added: Status enum for Feedback model - Added: Controller, DTO model for request, filter in admin list view

--- src/main/java/com/example/edutrack/curriculum/controller/MentorController.java
+++ src/main/java/com/example/edutrack/curriculum/controller/MentorController.java
@@ -1,9 +1,17 @@
 package com.example.edutrack.curriculum.controller;
 
+import com.example.edutrack.accounts.model.Mentee;
 import com.example.edutrack.accounts.model.Mentor;
+import com.example.edutrack.accounts.model.User;
+import com.example.edutrack.accounts.repository.MentorRepository;
 import com.example.edutrack.accounts.service.MentorService;
+import com.example.edutrack.curriculum.model.Course;
 import com.example.edutrack.curriculum.model.CourseMentor;
+import com.example.edutrack.curriculum.model.Feedback;
+import com.example.edutrack.curriculum.repository.CourseRepository;
 import com.example.edutrack.curriculum.service.implementation.CourseMentorServiceImpl;
+import com.example.edutrack.curriculum.service.interfaces.FeedbackService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -15,16 +23,24 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Controller(value = "mentee")
 public class MentorController {
     private final MentorService mentorService;
     private final CourseMentorServiceImpl courseMentorServiceImpl;
+    private final FeedbackService feedbackService;
+    private final CourseRepository courseRepository;
 
-    public MentorController(MentorService mentorService, CourseMentorServiceImpl courseMentorServiceImpl) {
+    public MentorController(MentorService mentorService,
+                            CourseMentorServiceImpl courseMentorServiceImpl,
+                            FeedbackService feedbackService,
+                            CourseRepository courseRepository) {
         this.mentorService = mentorService;
         this.courseMentorServiceImpl = courseMentorServiceImpl;
+        this.feedbackService = feedbackService;
+        this.courseRepository = courseRepository;
     }
 
     // View mentors in a list, directory: localhost:port/mentors
@@ -62,7 +78,6 @@ public class MentorController {
 
         Pageable pageable = PageRequest.of(page - 1, size_page, sort);
 
-        // Now, assuming your mentorService supports pagination:
         Page<Mentor> mentorPage = mentorService.searchMentors(
                 name, expertise, rating, totalSessions, isAvailable, pageable
         );
@@ -84,10 +99,40 @@ public class MentorController {
     public String viewMentorDetail(@PathVariable UUID id, Model model){
         List<CourseMentor> courseMentors = courseMentorServiceImpl.getCourseMentorByMentorId(id);
 
+        List<Feedback> feedbacks = feedbackService.getAllFeedbacksByMentorId(id);
+
+        model.addAttribute("feedbacks", feedbacks);
         model.addAttribute("mentor", mentorService.getMentorById(id));
         model.addAttribute("courses", courseMentors);
         model.addAttribute("subjects", mentorService.getTagsByMentor(id));
         model.addAttribute("cv", mentorService.getCVById(id));
         return "mentor-course";
+    }
+
+    @GetMapping("/mentors/{mentorId}/review")
+    public String showReviewForm(@PathVariable UUID mentorId,
+                                 @RequestParam UUID courseId,
+                                 HttpSession session,
+                                 Model model) {
+        Optional<Mentor> mentorOpt = mentorService.getMentorById(mentorId);
+
+        if (mentorOpt.isPresent()) {
+            Mentor mentor = mentorOpt.get();
+            model.addAttribute("mentor", mentor);
+        }
+
+        Optional<Course> courseOpt = courseRepository.findById(courseId);
+        if (courseOpt.isPresent()) {
+            Course course = courseOpt.get();
+            model.addAttribute("course", course);
+        }
+
+        Mentee mentee = (Mentee) session.getAttribute("loggedInUser");
+
+        UUID menteeId = mentee.getId();
+        model.addAttribute("menteeId", menteeId);
+        model.addAttribute("courseId", courseId);
+
+        return "mentee/mentor-review";
     }
 }
