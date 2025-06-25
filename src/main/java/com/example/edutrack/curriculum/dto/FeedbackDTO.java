764c8def90fb49102ad12f638f0a5739ed086ee2
nguyenhoanvn <hoanyu12345@gmail.com>
2025-06-26 05:27:15 +0700

    - Added: Status enum for Feedback model - Added: Controller, DTO model for request, filter in admin list view

---
+++ src/main/java/com/example/edutrack/curriculum/dto/FeedbackDTO.java
@@ -0,0 +1,62 @@
+package com.example.edutrack.curriculum.dto;
+
+import java.util.UUID;
+
+public class FeedbackDTO {
+    private String content;
+    private Double rating;
+    private UUID menteeId;
+    private UUID courseId;
+    private UUID mentorId;
+
+    public FeedbackDTO() {
+    }
+
+    public FeedbackDTO(String content, Double rating, UUID menteeId, UUID courseId, UUID mentorId) {
+        this.content = content;
+        this.rating = rating;
+        this.menteeId = menteeId;
+        this.courseId = courseId;
+        this.mentorId = mentorId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+    public UUID getMenteeId() {
+        return menteeId;
+    }
+
+    public void setMenteeId(UUID menteeId) {
+        this.menteeId = menteeId;
+    }
+
+    public UUID getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(UUID courseId) {
+        this.courseId = courseId;
+    }
+
+    public UUID getMentorId() {
+        return mentorId;
+    }
+
+    public void setMentorId(UUID mentorId) {
+        this.mentorId = mentorId;
+    }
+}
